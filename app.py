a2adac2be4810841f244549bc5b9a584f9aa55ae
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-12-20 21:50:09 -0800

    Initialize Streamlit app with wide layout and title

---
+++ app.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+# Set the wide layout of the web page
+st.set_page_config(layout="wide", page_title="WEN-OKN")
+
+# Set up the title
+st.markdown("### &nbsp; WEN-OKN: Dive into Data, Never Easier")
