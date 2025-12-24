24f73d8e8117de0075429520a1aaad7c04413c17
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-12-24 08:57:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -113,12 +113,28 @@ def scan_skills_documentation():
 
 # Initialize LLM
 def get_llm():
+
     return ChatOpenAI(
-        model="glm-4.6",
-        base_url="https://ellm.nrp-nautilus.io/v1",
-        api_key=os.environ.get("NRP_API_KEY"),
-        temperature=0,
+        model="mimo-v2-flash",
+        base_url="https://api.xiaomimimo.com/v1",
+        api_key=st.secrets["XIAOMI_API_KEY"],
+        temperature=st.session_state.get("temp_slider", 0.3),
+        top_p=0.95,
+        stream=False,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0,
+        extra_body={
+            "thinking": {"type": "disabled"}
+        }
     )
+
+    # return ChatOpenAI(
+    #     model="glm-4.6",
+    #     base_url="https://ellm.nrp-nautilus.io/v1",
+    #     api_key=os.environ.get("NRP_API_KEY"),
+    #     temperature=0,
+    # )
 
 # Function to display all GeoDataFrames as layers on a single map
 def display_all_layers_map():
