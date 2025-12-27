bebd72f7e7b36f18f5d787cfde1689d4f75eb696
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-12-26 16:03:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -975,6 +975,7 @@ if st.session_state.current_view == "sample":
                 """)
     st.markdown("#### Hydrologic Basins, Watersheds, and Stream Gages")
     st.markdown("""
+                * Find the hydrologic basin that contains the Scioto River outlet.
                 * Find all hydrological basins that intersect the state of Ohio.
                 * Find all counties that intersect the Lower Ohio–Salt Basin.
                 * Find all basins through which the Scioto River flows.
